724251652e4d16c778d7b5c5db4e938cddc5f11b
Josh McArthur <joshua.mcarthur@gmail.com>
2013-05-01 15:39:22 +1200

    Copy my global gitignore into place. Clone oh-my-zsh myself

--- modules/people/manifests/joshmcarthur.pp
+++ modules/people/manifests/joshmcarthur.pp
@@ -1,6 +1,7 @@
 class people::joshmcarthur {
 
   # Core boxen
+  include git
   include sublime_text_2
   include mou
   include spotify
@@ -27,9 +28,8 @@ class people::joshmcarthur {
 
   # Third-party
   include evernote # jasonamyers/puppet-evernote
-  include spectacle # dieterdemeyer/puppet-spectacle
+  include spectacle # jhubert/puppet-spectacle
   include gimp # fschwiet/puppet-gimp
-  include oh-my-zsh # pfeff/puppet-oh-my-zsh
   include calibre # agilecoders/puppet-calibre
 
   heroku::plugin { 'accounts':
@@ -38,50 +38,48 @@ class people::joshmcarthur {
 
   $home     = "/Users/${::luser}"
   $dotfiles ="${home}/.dotfiles"
+  $oh_my_zsh_root = "${home}/.oh-my-zsh"
 
   repository { $dotfiles:
     source => 'joshmcarthur/dotfiles',
   }
 
-  file { '${home}/.gemrc':
-    ensure: 'link',
-    target: "${dotfiles}/.gemrc",
-    require: Repository[$dotfiles]
+  repository { $oh_my_zsh_root:
+    source => 'robbyrussell/oh-my-zsh'
   }
 
-  file { '${home}/.irbrc':
-    ensure: 'link',
-    target: "${dotfiles}/.irbrc",
-    require: Repository[$dotfiles]
+  file { "${home}/.irbrc":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.irbrc",
+    require =>  Repository[$dotfiles]
   }
 
-  file { '${home}/.zshrc':
-    ensure: 'link',
-    target: "${dotfiles}/.zshrc",
-    require: Repository[$dotfiles]
+  file { "${home}/.zshrc":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.zshrc",
+    require =>  Repository[$dotfiles]
   }
 
-  file { '${home}/.gitconfig':
-    ensure: 'link',
-    target: "${dotfiles}/.gitconfig",
-    require: Repository[$dotfiles]
+  file { "${home}/.gitconfig":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.gitconfig",
+    require =>  Repository[$dotfiles]
   }
 
-  file { '${home}/.gitignore_global':
-    ensure: 'link',
-    target: "${dotfiles}/.gitignore_global",
-    require: Repository[$dotfiles]
+  # From: https://github.com/boxen/puppet-git/issues/6
+  Git::Config::Global <| title == 'core.excludesfile' |> {
+    value => "${dotfiles}/.gitignore_global"
   }
 
-  file { '${home}/.bundle':
-    ensure: 'link',
-    target: "${dotfiles}/.bundle",
-    require: Repository[$dotfiles]
+  file { "${home}/.bundle":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.bundle",
+    require =>  Repository[$dotfiles]
   }
 
-  file { '${home}/.oh-my-zsh/themes/sudojosh.zsh-theme':
-    ensure: 'link',
-    target: "${dotfiles}/sudojosh.zsh-theme",
-    require: Repository[$dotfiles]
+  file { "${home}/.oh-my-zsh/themes/sudojosh.zsh-theme":
+    ensure =>  'link',
+    target =>  "${dotfiles}/sudojosh.zsh-theme",
+    require =>  Repository[$dotfiles]
   }
 }

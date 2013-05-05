cc75370be3a8cdbabf5b83bd9b979f0cf6badb57
Charles Peach <charles@3months.com>
2013-05-05 20:11:16 +1200

    Add my personal manifest config

---
+++ modules/people/manifests/charlespeach.pp
@@ -0,0 +1,73 @@
+class people::charlespeach {
+
+  # Core boxen
+  include git
+  include sublime_text_2
+  include mou
+  include spotify
+  include adium
+  include alfred
+  include ccleaner
+  include vlc
+  include virtualbox
+  include mysql
+  include iterm2::stable
+  include skype
+
+  # Need to add this to .zshrc after install
+  # [[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
+
+  include autojump
+  include heroku
+  include chrome::canary
+  include postgresql
+  include zsh
+  include phantomjs
+  include onepassword
+  include fluid
+
+  # Third-party
+  include evernote # jasonamyers/puppet-evernote
+  include spectacle # jhubert/puppet-spectacle
+  include gimp # fschwiet/puppet-gimp
+  include calibre # agilecoders/puppet-calibre
+
+  heroku::plugin { 'accounts':
+    source => 'ddollar/heroku-accounts'
+  }
+
+  $home     = "/Users/${::luser}"
+  $dotfiles ="${home}/.dotfiles"
+  $oh_my_zsh_root = "${home}/.oh-my-zsh"
+
+  repository { $dotfiles:
+    source => 'charlespeach/dotfiles',
+  }
+
+  repository { $oh_my_zsh_root:
+    source => 'robbyrussell/oh-my-zsh'
+  }
+
+  file { "${home}/.irbrc":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.irbrc",
+    require =>  Repository[$dotfiles]
+  }
+
+  file { "${home}/.zshrc":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.zshrc",
+    require =>  Repository[$dotfiles]
+  }
+
+  file { "${home}/.gitconfig":
+    ensure =>  'link',
+    target =>  "${dotfiles}/.gitconfig",
+    require =>  Repository[$dotfiles]
+  }
+
+  # From: https://github.com/boxen/puppet-git/issues/6
+  Git::Config::Global <| title == 'core.excludesfile' |> {
+    value => "${dotfiles}/.gitignore_global"
+  }
+}
